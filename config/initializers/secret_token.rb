b3799da6b6a1633f18de90bc0330d36bc74e6046
asdf <asdfslkadslfasdf@Asdfslk-adslfasdfs-MacBook.local>
2012-12-10 01:08:30 -0500

    Initial commit - rails generated skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tst::Application.config.secret_token = '223a0de0e2d110ccc821d2819f4703bbc8d4847ae58ae0044c67a766998cef4e2202c17a15c18f66ce23ab8f60aee4f1ae88c3e9fb1c765820af957766d13895'
